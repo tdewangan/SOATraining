fe49328223ff38e5ec49b21395c7b585bc87c2f8
tdewangan <dewangan.tribhuwan@gmail.com>
2016-08-31 14:14:40 +0530

    Modify load balancing algorithm

--- OrdersLoadBalancer/src/main/java/com/example/OrderLoadBalancerApplication.java
+++ OrdersLoadBalancer/src/main/java/com/example/OrderLoadBalancerApplication.java
@@ -3,9 +3,12 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+//Change loadbalancing algorithm to ping
+@RibbonClient(configuration = CustomLoadBalancerConfiguration.class, name="lb1")
 public class OrderLoadBalancerApplication {
 
 	public static void main(String[] args) {
